584435c5dfa884aac96bc2f61cb53b7cb7a690ab
kcstarless <155853062+kcstarless@users.noreply.github.com>
2025-02-20 08:38:39 +1100

    updated

--- src/hooks/useMousePosition.jsx
+++ src/hooks/useMousePosition.jsx
@@ -4,13 +4,29 @@ const useMousePosition = () => {
   const [mousePosition, setMousePosition] = useState({ x: null, y: null });
 
   const updateMousePosition = (e) => {
-    setMousePosition({ x: e.clientX, y: e.clientY });
+    // If it's a mouse event, get the mouse position
+    if (e.type === "mousemove") {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    }
+    // If it's a touch event, get the first touch position
+    if (e.type === "touchmove") {
+      const touch = e.touches[0];
+      setMousePosition({ x: touch.clientX, y: touch.clientY });
+    }
   };
 
   useEffect(() => {
+    // For mouse events
     window.addEventListener("mousemove", updateMousePosition);
 
-    return () => window.removeEventListener("mousemove", updateMousePosition);
+    // For touch events (on touch screens)
+    window.addEventListener("touchmove", updateMousePosition);
+
+    // Clean up event listeners when the component unmounts
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+      window.removeEventListener("touchmove", updateMousePosition);
+    };
   }, []);
 
   return mousePosition;
