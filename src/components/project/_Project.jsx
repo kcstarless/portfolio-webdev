3f6978e45c3c53d13de51a8c2189d50e4c770fd0
kcstarless <155853062+kcstarless@users.noreply.github.com>
2025-03-16 04:40:05 +1100

    applied sticky to all section

--- src/components/project/_Project.jsx
+++ src/components/project/_Project.jsx
@@ -35,98 +35,98 @@ const Project = () => {
   };
 
   return (
-    <div className={styles.sticky_wrapper}>
-      <section id={styles.project}>
-        <div className={styles.project_header}>
-          <div className={styles.project_header_title}>PROJECTS</div>
-          <div className={styles.project_header_viewall}>
-            <div
-              className={styles.link}
-              onClick={handleClick}
-              data-text={isExpanding ? "←" : "→"}
-            >
-              <span>
-                {menuTextAnimation(isExpanding ? "VIEW LESS" : "VIEW MORE")}
-              </span>
-            </div>
+    // <div className={styles.sticky_wrapper}>
+    <section id={styles.project}>
+      <div className={styles.project_header}>
+        <div className={styles.project_header_title}>PROJECTS</div>
+        <div className={styles.project_header_viewall}>
+          <div
+            className={styles.link}
+            onClick={handleClick}
+            data-text={isExpanding ? "←" : "→"}
+          >
+            <span>
+              {menuTextAnimation(isExpanding ? "VIEW LESS" : "VIEW MORE")}
+            </span>
           </div>
         </div>
-        <div className={styles.projects_container}>
-          <div
-            className={`${styles.project_list} ${
-              isExpanding ? styles.active : ""
-            }`}
-          >
-            {projects()
-              .slice(-4)
-              .reverse()
-              .map((project, index) => (
-                <div key={index} className={styles.project_card}>
-                  <div className={styles.image_overlay}></div>
-                  <img src={project.thumb} className={styles.project_image} />
+      </div>
+      <div className={styles.projects_container}>
+        <div
+          className={`${styles.project_list} ${
+            isExpanding ? styles.active : ""
+          }`}
+        >
+          {projects()
+            .slice(-4)
+            .reverse()
+            .map((project, index) => (
+              <div key={index} className={styles.project_card}>
+                <div className={styles.image_overlay}></div>
+                <img src={project.thumb} className={styles.project_image} />
 
-                  <div className={styles.project_number}>
-                    <span className={styles.number}>{project.id}</span>
+                <div className={styles.project_number}>
+                  <span className={styles.number}>{project.id}</span>
+                </div>
+
+                <div className={styles.project_details}>
+                  <div className={styles.project_name}>{project.name}</div>
+                  <div className={styles.project_tech}>
+                    {project.tech_stack.map((tech, index) => (
+                      <span key={index}>
+                        <img
+                          src={tech.icon}
+                          alt={tech.name}
+                          className={styles.tech_icon}
+                        />
+                      </span>
+                    ))}
                   </div>
 
-                  <div className={styles.project_details}>
-                    <div className={styles.project_name}>{project.name}</div>
-                    <div className={styles.project_tech}>
-                      {project.tech_stack.map((tech, index) => (
-                        <span key={index}>
-                          <img
-                            src={tech.icon}
-                            alt={tech.name}
-                            className={styles.tech_icon}
-                          />
-                        </span>
-                      ))}
-                    </div>
+                  <div className={styles.project_desc}>{project.short}</div>
+                  <div className={styles.project_links}>
+                    <a href={project.site_link}>DEMO</a>
+                    <a href={project.github_link}>GITHUB</a>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
 
-                    <div className={styles.project_desc}>{project.short}</div>
-                    <div className={styles.project_links}>
-                      <a href={project.site_link}>DEMO</a>
-                      <a href={project.github_link}>GITHUB</a>
-                    </div>
+        <div
+          className={`${styles.project_listall_container} ${
+            isExpanding ? styles.active_all : ""
+          }`}
+        >
+          <div className={styles.project_listall}>
+            {projects()
+              .reverse()
+              .map((project, index) => (
+                <div
+                  key={index}
+                  className={styles.project_card}
+                  onMouseEnter={() => {
+                    setModal({ active: true, index });
+                  }}
+                  onMouseLeave={() => {
+                    setModal({ active: false, index });
+                  }}
+                  onClick={() => handleProjectLink(project.site_link)}
+                >
+                  <div className={styles.number}>{project.id}</div>
+                  <div className={styles.project_name}>{project.name}</div>
+                  <div className={styles.project_short}>{project.short}</div>
+                  <div className={styles.year_container}>
+                    <div className={styles.project_year}>{project.year}</div>
                   </div>
                 </div>
               ))}
           </div>
-
-          <div
-            className={`${styles.project_listall_container} ${
-              isExpanding ? styles.active_all : ""
-            }`}
-          >
-            <div className={styles.project_listall}>
-              {projects()
-                .reverse()
-                .map((project, index) => (
-                  <div
-                    key={index}
-                    className={styles.project_card}
-                    onMouseEnter={() => {
-                      setModal({ active: true, index });
-                    }}
-                    onMouseLeave={() => {
-                      setModal({ active: false, index });
-                    }}
-                    onClick={() => handleProjectLink(project.site_link)}
-                  >
-                    <div className={styles.number}>{project.id}</div>
-                    <div className={styles.project_name}>{project.name}</div>
-                    <div className={styles.project_short}>{project.short}</div>
-                    <div className={styles.year_container}>
-                      <div className={styles.project_year}>{project.year}</div>
-                    </div>
-                  </div>
-                ))}
-            </div>
-          </div>
         </div>
-        {/* <Modal modal={modal} projects={projects} /> */}
-      </section>
-    </div>
+      </div>
+      {/* <Modal modal={modal} projects={projects} /> */}
+    </section>
+    // </div>
   );
 };
 
