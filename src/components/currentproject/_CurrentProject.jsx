7766d9bc6188fc60a8b51b5ecca0243dd7976955
kcstarless <155853062+kcstarless@users.noreply.github.com>
2025-03-27 13:24:40 +1100

    updated currentproject

--- src/components/currentproject/_CurrentProject.jsx
+++ src/components/currentproject/_CurrentProject.jsx
@@ -1,56 +1,93 @@
 import styles from "./style.module.scss";
 import projects from "../project/projects";
-
-import { useState } from "react";
+import kcmVideo from "../../assets/videos/kcm_video.mp4";
+import { useState, useEffect, useRef } from "react";
 
 const Header = () => {
   const project = projects()[projects().length - 1];
   return (
     <div className={styles.header}>
-      <div className={styles.title}>Current Project</div>
-      <div className={styles.projectTitle}>{project.name}</div>
+      <div className={styles.projectTitle}>
+        <div className={styles.title}>Current Project</div>
+        {project.name}
+      </div>
     </div>
   );
 };
 
+// Overview, Details, Design, Challenges, Features, Outcome
 const Overview = ({ project }) => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+
+    const handleMouseEnter = () => {
+      video.play();
+    };
+
+    const handleMouseLeave = () => {
+      video.pause();
+      video.currentTime = 0; // Optional: Reset to the beginning
+    };
+
+    video.addEventListener("mouseenter", handleMouseEnter);
+    video.addEventListener("mouseleave", handleMouseLeave);
+
+    return () => {
+      video.removeEventListener("mouseenter", handleMouseEnter);
+      video.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, []);
+
   return (
     <div className={styles.overview}>
-      <div className={styles.item}>
-        <span className={styles.label}>Project No.</span>
-        <span className={styles.value}>{project.id}</span>
+      <div className={styles.separator}>
+        {/* Project Details */}
+        <div className={styles.item}>
+          <span className={styles.label}>Project No.</span>
+          <span className={styles.value}>{project.id}</span>
+        </div>
+        <div className={styles.item}>
+          <span className={styles.label}>Project Title</span>
+          <span className={styles.value}>{project.name}</span>
+        </div>
+        <div className={styles.item}>
+          <span className={styles.label}>Project Year</span>
+          <span className={styles.value}>{project.year}</span>
+        </div>
+        <div className={styles.item}>
+          <span className={styles.label}>Duration</span>
+          <span className={styles.value}>{project.duration}</span>
+        </div>
+        <div className={styles.item}>
+          <span className={styles.label}>Type</span>
+          <span className={styles.value}>{project.type}</span>
+        </div>
+        <div className={styles.item}>
+          <span className={styles.label}>Tech Stack</span>
+          <span className={styles.icon}>
+            {project.tech_stack.map((tech, index) => (
+              <span key={index} className={styles.techIcon}>
+                <img
+                  src={tech.icon}
+                  alt={tech.name}
+                  className={styles.techIcon}
+                />
+              </span>
+            ))}
+          </span>
+        </div>
       </div>
-
-      <div className={styles.item}>
-        <span className={styles.label}>Project Title</span>
-        <span className={styles.value}>{project.name}</span>
-      </div>
-
-      <div className={styles.item}>
-        <span className={styles.label}>Project Year</span>
-        <span className={styles.value}>{project.year}</span>
-      </div>
-
-      <div className={styles.item}>
-        <span className={styles.label}>Duration</span>
-        <span className={styles.value}>{project.duration}</span>
-      </div>
-
-      <div className={styles.item}>
-        <span className={styles.label}>Type</span>
-        <span className={styles.value}>{project.type}</span>
-      </div>
-
-      <div className={styles.item}>
-        <span className={styles.label}>Tech Stack</span>
-        <span className={styles.icon}>
-          {" "}
-          {project.tech_stack.map((tech, index) => (
-            <span key={index} className={styles.techIcon}>
-              <img src={tech.icon} alt={tech.name} class={styles.techIcon} />
-            </span>
-          ))}
-        </span>
+      <div className={styles.imageGrid}>
+        <video
+          ref={videoRef}
+          src={kcmVideo}
+          className={styles.video}
+          muted
+          playsInline
+          loop
+        />
       </div>
     </div>
   );
@@ -147,10 +184,10 @@ const CurrentProject = () => {
   return (
     <section id={styles.currentProject}>
       <div className={styles.mainContainer}>
-        <div className={styles.container}>
-          <Header />
-          <Project />
-        </div>
+        {/* <div className={styles.container}> */}
+        <Header />
+        <Project />
+        {/* </div> */}
       </div>
     </section>
   );
